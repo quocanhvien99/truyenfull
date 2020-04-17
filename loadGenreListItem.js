14bcd146d2eea9a5bf46392293f5357d86ac3934
quocanhvien99 <quocanhvien99@gmail.com>
2020-04-17 22:05:26 +0700

    Add fineshed-item component

---
+++ loadGenreListItem.js
@@ -0,0 +1,14 @@
+var itemList = document.getElementsByClassName('items');
+var count = 0;
+axios.get('/theloai.json').then(function(response) {
+    var render = response.data.map(function(x) {
+        if (count % 2 === 0) {
+            count++;
+            return '<div class="row"><div> <a href="#">' + x.content + '</a></div>';
+        } else {
+            count++;
+            return '<div> <a href="#">' + x.content + '</a></div></div>';
+        }        
+    }).join('');    
+    itemList[0].innerHTML = render;     
+});
